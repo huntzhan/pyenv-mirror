0cc7434a09762f44d357444da104d500b0d19da7
huntzhan <programmer.zhx@gmail.com>
2016-04-01 11:22:07 +0800

    Updated.

--- scripts/install-pyenv.sh
+++ scripts/install-pyenv.sh
@@ -7,9 +7,13 @@ PYENV_PACKAGE='pyenv-package.tar.gz'
 TMP_DIR=$(mktemp -d)
 
 # download files.
-wget "$PYTHON_BUILD_MIRROR_URL/$OFFLINE_INSTALL_SCRIPT" -P "$TMP_DIR"
-wget "$PYTHON_BUILD_MIRROR_URL/$PYENV_PACKAGE" -P "$TMP_DIR"
+wget "$PYTHON_BUILD_MIRROR_URL/scripts/$OFFLINE_INSTALL_SCRIPT" -P "$TMP_DIR"
+wget "$PYTHON_BUILD_MIRROR_URL/scripts/$PYENV_PACKAGE" -P "$TMP_DIR"
 
 # install.
 chmod +x "$TMP_DIR/$OFFLINE_INSTALL_SCRIPT"
 bash "$TMP_DIR/$OFFLINE_INSTALL_SCRIPT"
+
+# inform.
+echo "# download from local mirror."
+echo "export PYTHON_BUILD_MIRROR_URL=$PYTHON_BUILD_MIRROR_URL/pythons"
